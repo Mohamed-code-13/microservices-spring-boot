a221148c1f56efb6824be83f07b52825edcc9936
Mohamed-code-13 <mohamed.code.13@gmail.com>
2025-03-09 14:13:28 +0200

    Adding trending service

---
+++ trending-movies-service/src/main/java/com/example/trendingservice/resources/TrendingResource.java
@@ -0,0 +1,16 @@
+package com.example.trendingservice.resources;
+
+import com.example.trendingservice.models.Rating;
+import com.example.trendingservice.models.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/trending")
+public class TrendingResource {
+
+}
