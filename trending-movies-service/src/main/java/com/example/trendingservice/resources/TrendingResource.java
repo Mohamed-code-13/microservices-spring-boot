48c06fa7ee9e6d0f8bc0886b304ba02b935acf7c
Mohamed-code-13 <mohamed.code.13@gmail.com>
2025-03-09 18:17:39 +0200

    Implementing Trending.proto and GetTrendingMovies method

--- trending-movies-service/src/main/java/com/example/trendingservice/resources/TrendingResource.java
+++ trending-movies-service/src/main/java/com/example/trendingservice/resources/TrendingResource.java
@@ -1,21 +1,57 @@
 package com.example.trendingservice.resources;
 
-import com.example.grpc.HelloReply;
-import com.example.grpc.HelloRequest;
-import com.example.grpc.MyServiceGrpc;
+import com.example.grpc.Movie;
+import com.example.grpc.TrendingRequest;
+import com.example.grpc.TrendingResponse;
+import com.example.grpc.TrendingServiceGrpc;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
+import java.util.List;
+
 @GrpcService
-public class TrendingResource extends MyServiceGrpc.MyServiceImplBase {
+public class TrendingResource extends TrendingServiceGrpc.TrendingServiceImplBase {
 
     @Override
-    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-        HelloReply reply = HelloReply.newBuilder()
-                .setMessage("Hello: " + request.getName())
+    public void getTrendingMovies(TrendingRequest request, StreamObserver<TrendingResponse> responseObserver) {
+        List<Movie> movies = List.of(
+                Movie.newBuilder()
+                        .setName("Movie 1")
+                        .setDescription("Description 1")
+                        .setRating(4.4f)
+                        .build(),
+                Movie.newBuilder()
+                        .setName("Movie 2")
+                        .setDescription("Description 2")
+                        .setRating(2.2f)
+                        .build(),
+                Movie.newBuilder()
+                        .setName("Movie 3")
+                        .setDescription("Description 3")
+                        .setRating(5.0f)
+                        .build(),
+                Movie.newBuilder()
+                        .setName("Movie 4")
+                        .setDescription("Description 4")
+                        .setRating(1.4f)
+                        .build(),
+                Movie.newBuilder()
+                        .setName("Movie 5")
+                        .setDescription("Description 5")
+                        .setRating(0.4f)
+                        .build(),
+                Movie.newBuilder()
+                        .setName("Movie 6")
+                        .setDescription("Description 6")
+                        .setRating(0.0f)
+                        .build()
+        );
+
+        TrendingResponse response = TrendingResponse.newBuilder()
+                .addAllMovies(movies)
                 .build();
 
-        responseObserver.onNext(reply);
+        responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
 
